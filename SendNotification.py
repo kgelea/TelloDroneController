a0f1c0531913e7225b1ce0f72dac1c9ccbe56dfb
kknydnai <49187483+kknydnai@users.noreply.github.com>
2021-11-15 17:24:16 +0100

    Create SendNotification.py
    Created first version of send notification message

---
+++ SendNotification.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 15 17:14:59 2021
+
+@author: Daniel Kuknyo
+
+This script will be the template to send a notification to a phone via the Telegram API
+
+STEPS TO TAKE BEFORE:
+1) Get a Telegram account (free) using your phone number
+Web: https://web.telegram.org Also download the Telegram Android. 
+
+2) Go into settings (web or app) and set a username
+This is needed to obtain an id which your bot will use to send messages to.
+
+3) Send a message to RawDataBot to get your id
+Just search for RawDataBot and send any message (hi will do). Take a note of your id.
+
+4) Create your bot (which you'll command with HTTP requests)
+Now search for BotFather and send the message /start. Help is displayed. 
+Send the message /newbot and follow the instructions. 
+Take a note of your token to access the HTTP API
+"""
+
+import requests
+
+token = "2127036493:AAFzTiQxgRNerbsNAD__4NqpWyumUttImL0"
+
+url = f"https://api.telegram.org/bot{token}" # Here comes the token from step 4
+
+params = {"chat_id": "2140059741", "text": "There's a fucking fire get your ass out!"} # Here comes the token from step 3
+
+r = requests.get(url + "/sendMessage", params=params)
