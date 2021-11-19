664e9d58f22440fc850ef8a09d17403dec496148
kknydnai <49187483+kknydnai@users.noreply.github.com>
2021-11-19 15:21:34 +0100

    Updated notification sender with photo attachment possibility

--- SendNotification.py
+++ SendNotification.py
@@ -3,7 +3,6 @@
 Created on Mon Nov 15 17:14:59 2021
 
 @author: Daniel Kuknyo
-
 This script will be the template to send a notification to a phone via the Telegram API
 
 STEPS TO TAKE BEFORE:
@@ -25,14 +24,24 @@ TELEGRAM E-CHANNEL ID FOR WEDS
 Alina: 923197636
 Dani: 2140059741
 Sofien: 2132359615
+Mariam: 2144912667
 """
 
 import requests
+import cv2
 
-token = "2127036493:AAFzTiQxgRNerbsNAD__4NqpWyumUttImL0"
+def send_notifications(ids, text, photo):
+    token = "2127036493:AAFzTiQxgRNerbsNAD__4NqpWyumUttImL0"
+    url = f"https://api.telegram.org/bot{token}" # Here comes the token from step 4
+    
+    for idx in ids: # Send the message to each of the IDs gotten through the params
+        params = {"chat_id": idx, "text": text} # Here comes the token from step 3
+        r = requests.get(url + "/sendPhoto", params=params, files=photo)
+        print(r.status_code, r.reason, r.content)
 
-url = f"https://api.telegram.org/bot{token}" # Here comes the token from step 4
+#ids = ['923197636', '2140059741', '2132359615', '2144912667']
+ids = ['2140059741', '2144912667'] # This will contain the Telegram IDs to send messages to 
+text = 'Fire in the hole!' # What message should be sent
+photo = {'photo': open('10978489_347502588794003_74380988232417238_n.jpg', 'rb')} # Photo to attach to image
 
-params = {"chat_id": "923197636", "text": "Did u get this message?"} # Here comes the token from step 3
-
-r = requests.get(url + "/sendMessage", params=params)
+send_notifications(ids, text, photo)
