e62d12781cc8382cc3a7801588aeb829b55fbf7f
kknydnai <49187483+kknydnai@users.noreply.github.com>
2021-11-20 11:11:11 +0100

    Sending photo works to multiple users

--- SendNotification.py
+++ SendNotification.py
@@ -29,11 +29,12 @@ Mariam: 2144912667
 
 import requests
 
-def send_notifications(ids, text, photo):
+def send_notifications(ids, text, photoname):
     token = "2127036493:AAFzTiQxgRNerbsNAD__4NqpWyumUttImL0"
     url = f"https://api.telegram.org/bot{token}" # Here comes the token from step 4
     
     for idx in ids: # Send the message to each of the IDs gotten through the params
+        photo = {'photo': open(photoname, 'rb')} # Photo to attach to image    
         params = {"chat_id": idx, "text": text} # Here comes the token from step 3
         r = requests.get(url + "/sendPhoto", params=params, files=photo)
         print(r.status_code, r.reason, r.content)
@@ -41,6 +42,6 @@ def send_notifications(ids, text, photo):
 #ids = ['923197636', '2140059741', '2132359615', '2144912667']
 ids = ['2140059741', '2144912667'] # This will contain the Telegram IDs to send messages to 
 text = 'Fire in the hole!' # What message should be sent
-photo = {'photo': open('10978489_347502588794003_74380988232417238_n.jpg', 'rb')} # Photo to attach to image
+photoname = '10978489_347502588794003_74380988232417238_n.jpg'
 
-send_notifications(ids, text, photo)
+send_notifications(ids, text, photoname)
